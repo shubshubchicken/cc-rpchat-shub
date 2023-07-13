20e741a7b9b37a5b62ed101a8e1fd48f6159e759
shubshubchicken <139413339+shubshubchicken@users.noreply.github.com>
2023-07-12 21:32:25 -0400

    Update config.lua
    Config option for changes in the main.lua pull request

--- config.lua
+++ config.lua
@@ -9,3 +9,5 @@ config.connectionMessages = true          -- set this to true if you would like 
 config.antiSpam = true                    -- set this to true if you would like to use the cc chat antispam system                      
 
 config.DiscordWebhook = false             -- Set to your Discord Webhook URL 
+
+config.nearestPostal = false                -- Set this to true if you are using the nearest-postal resource (https://github.com/DevBlocky/nearest-postal/releases)
